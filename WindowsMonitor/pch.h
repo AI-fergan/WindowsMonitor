bf8df5f898d3a6215e9fdde5a962a38b3c54dc33
AI-fergan <ifrganvrcturh@gmail.com>
2024-03-21 23:06:24 +0200

    Added basic processes monitoring functions

---
+++ WindowsMonitor/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <tchar.h>
+#include <psapi.h>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
---
+++ WindowsMonitor/processes.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "pch.h"
+
+void PrintProcessNameAndID(DWORD processID);
+void PrintAllProcessesNameAndID();
